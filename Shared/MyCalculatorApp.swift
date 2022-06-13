1ed82b5d385ff0a327cfee365c8e87170243c74b
Vikas Kumar <vikas@myhealthcare.life>
2022-06-13 21:31:58 +0530

    Initial Commit

---
+++ Shared/MyCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCalculatorApp.swift
+//  Shared
+//
+//  Created by Vikas Kumar on 13/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MyCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
